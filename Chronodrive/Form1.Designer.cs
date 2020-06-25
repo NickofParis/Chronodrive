07ee6fd8d23d91bf649d12e75fd1be372d69e95b
NickofParis <ngingreau@gmail.com>
2020-06-25 21:51:59 +0200

    Add project files.

---
+++ Chronodrive/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Chronodrive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.openFileDialogTemplate = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelTemplateName = new System.Windows.Forms.Label();
+            this.buttonSelectTemplate = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numéro de Client :";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxId.Location = new System.Drawing.Point(199, 299);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(200, 30);
+            this.textBoxId.TabIndex = 3;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(21, 505);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 17);
+            this.labelStatus.TabIndex = 4;
+            // 
+            // openFileDialogTemplate
+            // 
+            this.openFileDialogTemplate.FileName = "openFileDialog1";
+            this.openFileDialogTemplate.Title = "Select Template File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Type de carte :";
+            // 
+            // labelTemplateName
+            // 
+            this.labelTemplateName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTemplateName.Location = new System.Drawing.Point(196, 228);
+            this.labelTemplateName.Name = "labelTemplateName";
+            this.labelTemplateName.Size = new System.Drawing.Size(155, 25);
+            this.labelTemplateName.TabIndex = 6;
+            this.labelTemplateName.Text = "Please Select";
+            // 
+            // buttonSelectTemplate
+            // 
+            this.buttonSelectTemplate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSelectTemplate.Location = new System.Drawing.Point(357, 223);
+            this.buttonSelectTemplate.Name = "buttonSelectTemplate";
+            this.buttonSelectTemplate.Size = new System.Drawing.Size(153, 35);
+            this.buttonSelectTemplate.TabIndex = 7;
+            this.buttonSelectTemplate.Text = "Selectionner";
+            this.buttonSelectTemplate.UseVisualStyleBackColor = true;
+            this.buttonSelectTemplate.Click += new System.EventHandler(this.buttonSelectTemplate_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(301, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 100);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Print";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(37, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(276, 136);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 533);
+            this.Controls.Add(this.buttonSelectTemplate);
+            this.Controls.Add(this.labelTemplateName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Impression Cartes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.OpenFileDialog openFileDialogTemplate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelTemplateName;
+        private System.Windows.Forms.Button buttonSelectTemplate;
+    }
+}
+
---
+++ Chronodrive/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zebra.Sdk.Card.Printer.Discovery;
+using Zebra.Sdk.Comm;
+using Zebra.Sdk.Card.Printer;
+using Zebra.Sdk.Card.Containers;
+using Zebra.Sdk.Card.Job.Template;
+using Zebra.Sdk.Printer.Discovery;
+using System.Threading;
+using System.Xml;
+using System.IO;
+
+namespace Chronodrive
+{
+    public partial class Form1 : Form
+    {
+        private const int CARD_FEED_TIMEOUT = 30000;
+        static string Value1;
+        static string Template;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxId.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxId.Text != "")
+            {
+                Value1 = textBoxId.Text;
+                ZebraCardPrinter zebraCardPrinter = null;
+                Connection connection = null;
+                String usbAdress = null;
+                try
+                {
+                    foreach (DiscoveredPrinter usbPrinter in UsbDiscoverer.GetZebraUsbPrinters(new ZebraCardPrinterFilter()))
+                    {
+                        usbAdress = usbPrinter.Address;
+                    }
+                    connection = new UsbConnection(usbAdress);
+                    connection.Open();
+
+                    zebraCardPrinter = ZebraCardPrinterFactory.GetInstance(connection);
+                    ZebraTemplate zebraCardTemplate = new ZebraCardTemplate(zebraCardPrinter);
+                    //string templateData = GetTemplateData();
+                    List<string> templateFields = zebraCardTemplate.GetTemplateDataFields(Template);
+                    Dictionary<string, string> fieldData = PopulateTemplateFieldData(templateFields);
+
+                    // Generate template job
+                    TemplateJob templateJob = zebraCardTemplate.GenerateTemplateDataJob(Template, fieldData);
+
+                    // Send job
+                    int jobId = zebraCardPrinter.PrintTemplate(1, templateJob);
+
+                    // Poll job status
+                    JobStatusInfo jobStatus = PollJobStatus(jobId, zebraCardPrinter);
+                    //labelStatus.Text = "Impression OK";
+                    //Console.WriteLine($"Job {jobId} completed with status '{jobStatus.PrintStatus}'.");
+                }
+                catch (Exception ev)
+                {
+                    labelStatus.Text = "Erreur d'impression : " + ev.Message;
+                    //Console.WriteLine($"Error printing template: {ev.Message}");
+                }
+                finally
+                {
+                    CloseQuietly(connection, zebraCardPrinter);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pas de valeur");
+            }
+
+
+        }
+        #region Template
+        private static string GetTemplateData()
+        {
+            return "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+                   "<template name=\"TemplateExample\" card_type=\"0\" card_thickness=\"40\" delete=\"yes\" source=\"feeder\" destination=\"eject\">\r\n" +
+                   "  <fonts>\r\n" +
+                   "    <font id=\"1\" name=\"Arial\" size=\"8\" bold=\"no\" italic=\"no\" underline=\"no\"/>\r\n" +
+                   "    <font id=\"2\" name=\"Arial\" size=\"6\" bold=\"no\" italic=\"no\" underline=\"no\"/>\r\n" +
+                   "    <font id=\"3\" name=\"Arial\" size=\"10\" bold=\"yes\" italic=\"no\" underline=\"no\"/>\r\n" +
+                   "  </fonts>\r\n" +
+                   "  <sides>\r\n" +
+                   "    <side name=\"front\" orientation=\"portrait\" rotation=\"180\" k_mode=\"mixed\">\r\n" +
+                   "      <print_types>\r\n" +
+                   "        <print_type type=\"monochrome\">\r\n" +
+                   "          <text field=\"\" font_id=\"1\" x=\"40\" y=\"20\" width=\"0\" height=\"0\" angle=\"0\" color=\"0x000000\">Votre numero de client:</text>\r\n" +
+                   "          <text field=\"\" font_id=\"1\" x=\"40\" y=\"346\" width=\"0\" height=\"0\" angle=\"0\" color=\"0x000000\">Votre numero de client:</text>\r\n" +
+                   "          <text field=\"\" font_id=\"1\" x=\"40\" y=\"694\" width=\"0\" height=\"0\" angle=\"0\" color=\"0x000000\">Votre numero de client:</text>\r\n" +
+                   "          <text field=\"\" font_id=\"2\" x=\"10\" y=\"276\" width=\"0\" height=\"0\" angle=\"0\" color=\"0x000000\">Ce code barre vous sert pour vous identifier a la borne</text>\r\n" +
+                   "          <text field=\"\" font_id=\"2\" x=\"10\" y=\"624\" width=\"0\" height=\"0\" angle=\"0\" color=\"0x000000\">Ce code barre vous sert pour vous identifier a la borne</text>\r\n" +
+                   "          <text field=\"\" font_id=\"2\" x=\"10\" y=\"950\" width=\"0\" height=\"0\" angle=\"0\" color=\"0x000000\">Ce code barre vous sert pour vous identifier a la borne</text>\r\n" +
+                   "          <text field=\"clientid\" font_id=\"3\" x=\"420\" y=\"20\" width=\"0\" height=\"0\" angle=\"0\" color=\"0x000000\"/>\r\n" +
+                   "          <text field=\"clientid\" font_id=\"3\" x=\"420\" y=\"346\" width=\"0\" height=\"0\" angle=\"0\" color=\"0x000000\"/>\r\n" +
+                   "          <text field=\"clientid\" font_id=\"3\" x=\"420\" y=\"694\" width=\"0\" height=\"0\" angle=\"0\" color=\"0x000000\"/>\r\n" +
+                   "          <barcode field=\"clientid\" code=\"code128\" x=\"140\" y=\"84\" width=\"420\" height=\"150\" bar_height=\"150\" multiplier=\"8\"/>\r\n" +
+                   "          <barcode field=\"clientid\" code=\"code128\" x=\"140\" y=\"422\" width=\"420\" height=\"150\" bar_height=\"150\" multiplier=\"8\"/>\r\n" +
+                   "          <barcode field=\"clientid\" code=\"code128\" x=\"140\" y=\"770\" width=\"420\" height=\"150\" bar_height=\"150\" multiplier=\"8\"/>\r\n" +
+                   "        </print_type>\r\n" +
+                   "      </print_types>\r\n" +
+                   "    </side>\r\n" +
+                   "  </sides>\r\n" +
+                   "</template>\r\n";
+        }
+        // <exception cref="ArgumentException"></exception>
+        /// <exception cref="System.IO.IOException"></exception>
+        /// <exception cref="Zebra.Sdk.Card.Exceptions.ZebraCardException"></exception>
+        /// <exception cref="Zebra.Sdk.Device.ZebraIllegalArgumentException"></exception>
+        private static Dictionary<string, string> PopulateTemplateFieldData(List<string> templateFields)
+        {
+            Dictionary<string, string> fieldData = new Dictionary<string, string>();
+            foreach (string fieldName in templateFields)
+            {
+                string fieldValue = "";
+                switch (fieldName)
+                {
+                    case "employee":
+                        fieldValue = Value1;
+                        break;
+
+                    case "clientid":
+                        fieldValue = Value1;
+                        break;
+
+                }
+
+                if (!string.IsNullOrEmpty(fieldValue))
+                {
+                    if (!fieldData.ContainsKey(fieldName))
+                    {
+                        fieldData.Add(fieldName, fieldValue);
+                    }
+                }
+            }
+            return fieldData;
+        }
+        #endregion Template
+
+        #region JobStatus
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ConnectionException"></exception>
+        /// <exception cref="IOException"></exception>
+        /// <exception cref="OverflowException"></exception>
+        /// <exception cref="Zebra.Sdk.Settings.SettingsException"></exception>
+        /// <exception cref="Zebra.Sdk.Card.Exceptions.ZebraCardException"></exception>
+        private static JobStatusInfo PollJobStatus(int jobId, ZebraCardPrinter zebraCardPrinter)
+        {
+            JobStatusInfo jobStatusInfo = new JobStatusInfo();
+            bool isFeeding = false;
+
+            long start = Math.Abs(Environment.TickCount);
+            while (true)
+            {
+                jobStatusInfo = zebraCardPrinter.GetJobStatus(jobId);
+
+                if (!isFeeding)
+                {
+                    start = Math.Abs(Environment.TickCount);
+                }
+
+                isFeeding = jobStatusInfo.CardPosition.Contains("feeding");
+
+                string alarmDesc = jobStatusInfo.AlarmInfo.Value > 0 ? $" ({jobStatusInfo.AlarmInfo.Description})" : "";
+                string errorDesc = jobStatusInfo.ErrorInfo.Value > 0 ? $" ({jobStatusInfo.ErrorInfo.Description})" : "";
+
+                //Console.WriteLine($"Job {jobId}: status:{jobStatusInfo.PrintStatus}, position:{jobStatusInfo.CardPosition}, alarm:{jobStatusInfo.AlarmInfo.Value}{alarmDesc}, error:{jobStatusInfo.ErrorInfo.Value}{errorDesc}");
+
+                if (jobStatusInfo.PrintStatus.Contains("done_ok"))
+                {
+                    break;
+                }
+                else if (jobStatusInfo.PrintStatus.Contains("error") || jobStatusInfo.PrintStatus.Contains("cancelled"))
+                {
+                    //Console.WriteLine($"The job encountered an error [{jobStatusInfo.ErrorInfo.Description}] and was cancelled.");
+                    break;
+                }
+                else if (jobStatusInfo.ErrorInfo.Value > 0)
+                {
+                    //Console.WriteLine($"The job encountered an error [{jobStatusInfo.ErrorInfo.Description}] and was cancelled.");
+                    zebraCardPrinter.Cancel(jobId);
+                }
+                else if (jobStatusInfo.PrintStatus.Contains("in_progress") && isFeeding)
+                {
+                    if (Math.Abs(Environment.TickCount) > start + CARD_FEED_TIMEOUT)
+                    {
+                        //Console.WriteLine("The job timed out waiting for a card and was cancelled.");
+                        zebraCardPrinter.Cancel(jobId);
+                    }
+                }
+
+                Thread.Sleep(2000);
+            }
+            return jobStatusInfo;
+        }
+        #endregion JobStatus
+
+        #region CleanUp
+        private static void CloseQuietly(Connection connection, ZebraCardPrinter zebraCardPrinter)
+        {
+            try
+            {
+                if (zebraCardPrinter != null)
+                {
+                    zebraCardPrinter.Destroy();
+                }
+            }
+            catch { }
+
+            try
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+            catch { }
+        }
+        #endregion CleanUp
+
+        private void buttonSelectTemplate_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialogTemplate = new OpenFileDialog {
+            InitialDirectory = "C:\\Zebra\\ZC\\",
+            CheckFileExists = true,
+            CheckPathExists = true,
+            DefaultExt = "xml",
+            Filter = "xml files (*.xml)|*.xml",
+            FilterIndex = 2,
+            RestoreDirectory = true
+            };
+
+            if (openFileDialogTemplate.ShowDialog() == DialogResult.OK)
+            {
+                labelTemplateName.Text = openFileDialogTemplate.SafeFileName;
+            }
+
+            Template = File.ReadAllText(openFileDialogTemplate.FileName, Encoding.UTF8);
+            textBoxId.Enabled = true;
+            if(openFileDialogTemplate.SafeFileName == "Employee.xml")
+            {
+                label1.Text = "Employé :";
+                textBoxId.Width = 300;
+                textBoxId.Left = 149;
+            }
+            
+        }
+    }
+}
